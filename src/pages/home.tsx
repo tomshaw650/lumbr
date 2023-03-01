b2acd193296eb9f774707d2d007715059056cf9d
Tom Shaw <tomshaw650@gmail.com>
2023-03-01 12:11:22 +0000

    updated view for loggedin/not

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,23 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 import NavBar from "../components/NavBar";
+import { useEffect } from "react";
 
 const Home = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
   const { data, isLoading } = trpc.user.getUser.useQuery();
 
+  useEffect(() => {
+    if (!isLoading && !data?.username) {
+      router.push("/auth/new-user");
+    }
+  }, [data, isLoading, router]);
+
   if (status === "loading" && isLoading) {
     return <main>Loading...</main>;
-  }
-
-  if (!isLoading && !data?.username) {
-    router.push("/auth/new-user");
   }
 
   if (!session) {
@@ -22,6 +26,9 @@ const Home = () => {
 
   return (
     <div>
+      <Head>
+        <title>Lumbr | Home</title>
+      </Head>
       <NavBar user={data} />
     </div>
   );
