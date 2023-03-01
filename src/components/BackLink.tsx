056e8721c321323a5d57510545e82fd8c3121f4e
Tom Shaw <tomshaw650@gmail.com>
2023-03-01 12:08:58 +0000

    BackLink component created

---
+++ src/components/BackLink.tsx
@@ -0,0 +1,23 @@
+// create backlink component
+import React from "react";
+import Link from "next/link";
+import { IoIosArrowForward } from "react-icons/io";
+
+interface Props {
+  href: string;
+  text: string;
+  current: boolean;
+}
+
+const BackLink: React.FC<Props> = React.memo(({ href, text, current }) => {
+  return (
+    <Link href={href}>
+      <div className="flex items-center">
+        <p className="text-md text-primary underline hover:font-bold">{text}</p>
+        {current ? null : <IoIosArrowForward />}
+      </div>
+    </Link>
+  );
+});
+
+export default BackLink;
